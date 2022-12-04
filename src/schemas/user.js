c5163fdc92994add51b4f2c75c7802faedfc61c0
Glitch (branch-important-coal) <none>
2022-12-04 06:53:04 +0000

    👗👜 Checkpoint ./src/schemas/user.js:50435260/204

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -4,6 +4,11 @@ const schema = Schema({
   userID: { type: String, default: "" },
   codes: { type: Array, default: [] },
   getLikeCount: { type: Number, default: 0 }
+  biography: {type: String, default: null},
+website: {type: String, default: null},
+github: {type: String, default: null},
+twitter: {type: String, default: null},
+instagram: {type: String, default: null}
 });
 
 module.exports = model("user", schema);
